2d0fbdf5eb96dc4849fb3fb4452f760adc27cab9
Alexej_Barzykin <Alexej_Barzykin@epam.com>
2022-06-08 00:16:05 +0400

    03-deploy-war-with-tomcat

--- src/main/java/com/barzykin/Hello.java
+++ src/main/java/com/barzykin/Hello.java
@@ -6,15 +6,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
 
 public class Hello extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String method = req.getMethod();
+        Enumeration<String> headerNames = req.getHeaderNames();
+        String requestURI = req.getRequestURI();
+        String protocol = req.getProtocol();
         resp.setContentType("text/html");
         resp.setCharacterEncoding("UTF-8");
         PrintWriter writer = resp.getWriter();
-        writer.write("<p><span style='color: blue;'>Привет, мир!</span></p>");
+        writer.write("<p><span style='color: green;'>Привет, мир!</span></p>");
     }
 
 }

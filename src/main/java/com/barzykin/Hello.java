ab7a8a30e2ce4227a3919a241b02735c5efc1e13
Alexej_Barzykin <Alexej_Barzykin@epam.com>
2022-06-08 00:33:15 +0400

    04-without-web-xml

--- src/main/java/com/barzykin/Hello.java
+++ src/main/java/com/barzykin/Hello.java
@@ -1,6 +1,7 @@
 package com.barzykin;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -8,6 +9,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Enumeration;
 
+@WebServlet(name = "HelloServlet", urlPatterns = "/hello")
 public class Hello extends HttpServlet {
 
     @Override
@@ -19,7 +21,7 @@ public class Hello extends HttpServlet {
         resp.setContentType("text/html");
         resp.setCharacterEncoding("UTF-8");
         PrintWriter writer = resp.getWriter();
-        writer.write("<p><span style='color: green;'>Привет, мир!</span></p>");
+        writer.write("<p><span style='color: red;'>Привет, мир!</span></p>");
     }
 
 }

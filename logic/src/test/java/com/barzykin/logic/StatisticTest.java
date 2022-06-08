6ae3db2c926af833eea3a8ba34e09587669bb431
Alexej_Barzykin <Alexej_Barzykin@epam.com>
2022-06-09 00:34:08 +0400

    07-junit5

--- logic/src/test/java/com/barzykin/logic/StatisticTest.java
+++ logic/src/test/java/com/barzykin/logic/StatisticTest.java
@@ -1,27 +1,31 @@
 package com.barzykin.logic;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.stream.Stream;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
-public class StatisticTest {
+
+class StatisticTest {
     Statistic statistic;
 
-    @Before
-    public void setUp() throws Exception {
+    @BeforeEach
+    public void setUp() {
         statistic = new Statistic();
     }
 
-    @Test
-    public void averageOfTwoExpectedInteger() {
-        //дано given
-        List<Integer> integers = List.of(6, 8);
-        BigDecimal expectedAverage = new BigDecimal("7.00");
+    @ParameterizedTest
+    @MethodSource("intIntBigDecimal")
+    void average(int a, int b, BigDecimal expectedAverage) {
+        List<Integer> integers = List.of(a, b);
 
         //выполнение when
         BigDecimal actualAverage = statistic.average(integers);
@@ -30,16 +34,11 @@ public class StatisticTest {
         assertEquals(expectedAverage, actualAverage);
     }
 
-    @Test
-    public void averageOfTwoExpectedDecimal() {
-        //дано given
-        List<Integer> integers = List.of(6, 7);
-        BigDecimal expectedAverage = new BigDecimal("6.50");
-
-        //выполнение when
-        BigDecimal actualAverage = statistic.average(integers);
-
-        //тогда, результат then
-        assertEquals(expectedAverage, actualAverage);
+    public static Stream<Arguments> intIntBigDecimal() {
+        return Stream.of(
+                arguments(6, 8, new BigDecimal("7.00")),
+                arguments(6, 7, new BigDecimal("6.50"))
+        );
     }
+
 }
